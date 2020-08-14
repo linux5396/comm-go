7a6587ab83c90c2259a08c6efa03c3285fab90a0
linux5396 <929159338@qq.com>
2020-08-15 02:22:10 +0800

    stack test

---
+++ container/stack_test.go
@@ -0,0 +1,20 @@
+package lru_cache
+
+import "testing"
+
+type Holder struct {
+	Stack
+}
+
+func TestNonSynchronizeStack(t *testing.T) {
+	h := Holder{GetNonSynchronizeStack(8)}
+	h.Push(1)
+	t.Log(h.Size())  //1
+	t.Log(h.Peek())  //1
+	t.Log(h.Empty()) //false
+	t.Log(h.Pop())
+	t.Log(h.Full())
+	for i := 0; i < 9; i++ {
+		t.Log(h.Push(i))
+	}
+}
